6c6e2fd5664fef9ac1b6f1b6eec0239d77746248
Pepe Iborra <pepeiborra@gmail.com>
2018-01-23 00:25:06 +0000

    Remove redundant imports and apply hlint suggestion

--- test/Hoed.hs
+++ test/Hoed.hs
@@ -9,9 +9,6 @@
 module Hoed where
 
 import qualified Data.ByteString.Lazy as B
-import qualified Data.List as List
-import Data.Data
-import Data.Monoid
 import Debug.Record hiding (getDebugTrace)
 import Debug.Hoed
 import System.Exit
@@ -25,7 +22,7 @@ debug [d|
 
     partition               :: (a -> Bool) -> [a] -> ([a],[a])
     {-# INLINE partition #-}
-    partition p xs = foldr (select p) ([],[]) xs
+    partition p = foldr (select p) ([],[])
 
     select :: (a -> Bool) -> a -> ([a], [a]) -> ([a], [a])
     select p x ~(ts,fs) | p x       = (x:ts,fs)
