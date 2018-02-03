7bb2ad7e700e4c0eb0e2ebaefed41e5227b7cfe4
marklnichols <marklnichols@gmail.com>
2018-02-03 14:14:12 -0500

    Removed some dead code

--- src/Debug/Util.hs
+++ src/Debug/Util.hs
@@ -16,18 +16,6 @@ import           Data.Maybe
 import           Language.Haskell.TH
 import           Language.Haskell.TH.Syntax
 
-{-
--- | Discover the function name inside (possibly nested) let expressions
---   Transform strings of the form "let (var tag "f" -> f) = f x in f_1" into "f'"
---   Each level of nesting gets a ' (prime) appeneded to the name
-removeLet :: String -> String
-removeLet str = loop "" str where
-   loop suffix s = if "let" `isInfixOf` fst (word1 s)
-        then case stripInfix " = " s of
-            Just pair -> loop ('\'' : suffix) (snd pair)
-            Nothing   -> s    -- this shouldn't happen...
-        else fst (word1 s) ++ suffix
--}
 -- | Discover the function name inside (possibly nested) let expressions
 --   Transform strings of the form "let (var tag "f" -> f) = f x in f_1" into "f"
 removeLet :: String -> String
